9edad5794e7ccabf39a16657f13e1569475bcf25
pmsk98 <pmsk98@naver.com>
2021-02-22 16:13:36 +0900

    stock_movement

---
+++ stock_gui.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 21:22:02 2020
+
+@author: user
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('stock_indicator')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
